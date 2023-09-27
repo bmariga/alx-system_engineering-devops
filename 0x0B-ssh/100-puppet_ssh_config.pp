e20cc600be89b1b81333d5571a064150aa445f95
mariga <mariga.bn@gmail.com>
2023-09-27 16:20:20 -0700

    client configuration file with Puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,14 @@
 # Client configuration file with Puppet
-file_line {  'passw_no_autentication':
-  path => '/etc/ssh/ssh_config',
-  line => 'PasswordAuthentication no',
-}
-file_line {  'passw_direction':
-  path => '/etc/ssh/ssh_config',
-  line => 'IdentityFile ~/.ssh/school',
+# Puppet to make changes in or config file
+
+file { '/etc/ssh/ssh_config';
+	ensure => present,   
+
+content =>"
+
+	#SSH client config
+	Host* 
+	IdentityFile ~/.ssh/school
+	PasswordAuthentication no 
+",
 }
